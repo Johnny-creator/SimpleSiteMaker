5c66a29be76171c18ff40fa8ddc98c603a67f369
Jonathan Nanno <jon.nanno@gmail.com>
2022-04-05 14:27:29 -0400

    Created a new form class that contains the form for user login

--- webForms.py
+++ webForms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, FileField, BooleanField
 from wtforms.validators import Length
 
-class WebsiteForm(FlaskForm):
+class pageCreationForm(FlaskForm):
     image = FileField("image")
     fullName1 = StringField("Full name", validators=[Length(min=4, max=25)])
     firstSect1 = StringField("First Section", validators=[Length(min=4, max=25)])
@@ -14,3 +14,9 @@ class WebsiteForm(FlaskForm):
     thirdText1 = TextAreaField("Third Text", validators=[Length(min=4)])
     fourthText1 = TextAreaField("Fourth Text", validators=[Length(min=4)])
     submit = SubmitField("Generate Website")
+    submit2 = SubmitField("Cancel")
+
+class userLoginForm(FlaskForm):
+    userName = StringField("User Name", validators=[Length(min=3, max=30)])
+    passWord = StringField("Password", validators=[Length(min=5, max=50)])
+    submit = SubmitField("Create User")
